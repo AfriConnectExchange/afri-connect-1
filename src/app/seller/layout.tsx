f3b7ad21850731697c8aeca58a166808cf76f89f
Pred Finance <predfi.xyz@gmail.com>
2025-10-04 21:04:53 +0000

    enough padding

--- src/app/seller/layout.tsx
+++ src/app/seller/layout.tsx
@@ -45,7 +45,7 @@ export default function SellerLayout({
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <SellerSidebar />
       <div className="flex flex-col">
-        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 bg-muted/40">
+        <main className="flex flex-1 flex-col gap-4 p-6 md:gap-8 md:p-10 bg-muted/40">
           {children}
         </main>
       </div>
